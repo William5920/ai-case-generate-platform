ca6e39c8adde2fc8cfb6a0d9f39188d645ddc175
William5920 <1101044179@qq.com>
2026-06-18 16:50:42 +0800

    调整后端启动脚本，开启热更新

--- backend/start_server.py
+++ backend/start_server.py
@@ -6,4 +6,4 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "libs"))
 import uvicorn
 from app.main import app
 
-uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
